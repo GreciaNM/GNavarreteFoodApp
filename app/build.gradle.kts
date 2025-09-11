eead24a1c26dcb37394a35533308c81998640d6b
GreciaNM <gnavarretemexicano@gmail.com>
2025-09-10 20:00:19 -0600

    chore: proyecto de examen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,28 +27,43 @@ android {
             )
         }
     }
+
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // BOM de Compose
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // ✅ Foundation (para LazyVerticalGrid, etc.)
+    implementation("androidx.compose.foundation:foundation")
+
+    // ✅ Íconos extendidos (estrella, etc.)
+    implementation("androidx.compose.material:material-icons-extended")
+
+    // ✅ Glide en Compose (Landscapist)
+    implementation("com.github.skydoves:landscapist-glide:2.3.6")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
